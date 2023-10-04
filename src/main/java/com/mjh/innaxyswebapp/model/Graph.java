e3b737317cdc25965d4e74e34f2942ea7cad067d
MaxMJH <MaxHarrisMJH@gmail.com>
2023-10-04 14:40:51 +0100

    Reverse the order of the shortest path to go from source to target rather than target to source.

--- src/main/java/com/mjh/innaxyswebapp/model/Graph.java
+++ src/main/java/com/mjh/innaxyswebapp/model/Graph.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
@@ -59,9 +60,15 @@ public class Graph {
 	}
 	
 	public List<Node> shortestPath(Node source, Node target) {
+		// Map to store all nodes and their distances from the source.
 		Map<Node, Integer> distances = new HashMap<>(this.nodes.size());
+		
+		// Map to store all nodes leading up to the source.
 		Map<Node, Node> previous = new HashMap<>(this.nodes.size());
+		
+		// Priority queue to store nodes that need to have their shortest distance checked from source.
 		Queue<Node> queue = new PriorityQueue<>(new Comparator<Node>() {
+			// Orders the queue based on the smallest distance from source.
 			@Override
 			public int compare(Node o1, Node o2) {
 				int o1Distance = distances.get(o1);
@@ -70,38 +77,64 @@ public class Graph {
 			}
 		});
 		
+		// Iterate through each node in the graph.
 		for(Node node : this.nodes) {
+			// Set their distance to the highest possible value to signify infinity.
 			distances.put(node, Integer.MAX_VALUE);
+			
+			// Set their place in nodes leading up to source.
 			previous.put(node, null);
+			
+			// Add the node to the queue.
 			queue.add(node);
 		}
 		
+		// Set the source node's distance to 0.
 		distances.put(source, 0);
 		
+		// Iterate through the queue until it is empty (all nodes have their distances calculated from source).
+		// Effectively calculates the shortest path from the source node to all other nodes.
 		while(!queue.isEmpty()) {
+			// Get the smallest distance node from the queue.
 			Node currentNode = queue.poll();
 			
+			// Get adjacent nodes from the current node (via the adjacency list).
 			for(Node neighbour : this.adjacencyList.get(currentNode).keySet()) {
+				// Calculate the distance from the current node to the neighbouring node.
 				int length = distances.get(currentNode) + this.adjacencyList.get(currentNode).get(neighbour);
 				
+				// Check to see if the calculated length is smaller than the current stored length (i.e. set the shortest path).
 				if(length < distances.get(neighbour)) {
+					// Set the distance from the current node to the current neighbour node.
 					distances.put(neighbour, length);
+					
+					// Add to the previous visited nodes map.
 					previous.put(neighbour, currentNode);
+					
+					// Update the shortest path for the current neighbouring node in the queue.
 					queue.remove(neighbour);
 					queue.add(neighbour);
 				}	
 			}
 		}
 		
+		// List to store the shortest path from source to target.
 		List<Node> path = new ArrayList<>();
 		
+		// Check to see if the target node exists in previously visited nodes map, or is equal to source.
 		if(previous.get(target) != null || target == source) {
+			// Iterate through each node that contains the target as its key.
 			while(target != null) {
+				// Add the node to the path, then get the next shortest node until the target is reached.
 				path.add(target);
 				target = previous.get(target);
 			}
 		}
 		
+		// As the algorithm returns the path from target to source, reverse the list.
+		Collections.reverse(path);
+		
+		// Return the shortest path from source to target.
 		return path;
 	}
 	
