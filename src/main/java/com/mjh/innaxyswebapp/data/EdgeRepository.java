a1eff501155d17e63434fc34112ff5f15c076ddc
MaxMJH <MaxHarrisMJH@gmail.com>
2023-10-07 17:20:07 +0100

    Adds JavaDoc comments to the repository classes, also changes EdgeRepository parameter from String to Long.

--- src/main/java/com/mjh/innaxyswebapp/data/EdgeRepository.java
+++ src/main/java/com/mjh/innaxyswebapp/data/EdgeRepository.java
@@ -4,6 +4,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mjh.innaxyswebapp.model.Edge;
 
-public interface EdgeRepository extends JpaRepository<Edge, String> {
+/**
+ * This interface allows for communication with the PostgreSQL database. 
+ * It is likely for this particular class to be utilised via a service class
+ * so as to apply CRUD. Further, if a primary key that is passed exists
+ * within the relevant table, a {@link com.mjh.innaxyswebapp.model.Edge} will
+ * be returned, populated with stored data.
+ * 
+ * @param <Edge> Returning node if the primary key value exists.
+ * @param <Long> Primary key of a relevant edge (ID of the edge).
+ */
+public interface EdgeRepository extends JpaRepository<Edge, Long> {
 
 }
--- src/main/java/com/mjh/innaxyswebapp/data/NodeRepository.java
+++ src/main/java/com/mjh/innaxyswebapp/data/NodeRepository.java
@@ -4,6 +4,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mjh.innaxyswebapp.model.Node;
 
+/**
+ * This interface allows for communication with the PostgreSQL database. 
+ * It is likely for this particular class to be utilised via a service class
+ * so as to apply CRUD. Further, if a primary key that is passed exists
+ * within the relevant table, a {@link com.mjh.innaxyswebapp.model.Node} will
+ * be returned, populated with stored data.
+ * 
+ * @param <Node>   Returning node if the primary key value exists.
+ * @param <String> Primary key of a relevant node (name of the node).
+ */
 public interface NodeRepository extends JpaRepository<Node, String> {
 
 }
