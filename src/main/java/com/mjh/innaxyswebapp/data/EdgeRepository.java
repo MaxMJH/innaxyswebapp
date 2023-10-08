a376ee22bcf9bf7269e232ab49adadd3fd4a4b0e
MaxMJH <MaxHarrisMJH@gmail.com>
2023-10-08 10:49:45 +0100

    Adds custom queries to both EdgeRepository and NodeRepository to be used in their respective service classes.

--- src/main/java/com/mjh/innaxyswebapp/data/EdgeRepository.java
+++ src/main/java/com/mjh/innaxyswebapp/data/EdgeRepository.java
@@ -1,6 +1,12 @@
 package com.mjh.innaxyswebapp.data;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.mjh.innaxyswebapp.model.Edge;
 
@@ -15,5 +21,65 @@ import com.mjh.innaxyswebapp.model.Edge;
  * @param <Long> Primary key of a relevant edge (ID of the edge).
  */
 public interface EdgeRepository extends JpaRepository<Edge, Long> {
-
+	/**
+	 * Method to find edges based on source node.
+	 * 
+	 * @param source The source node.
+	 */
+	@Modifying
+    @Transactional
+    @Query(value = "SELECT e FROM edges e WHERE e.source = ?1", nativeQuery = true)
+    List<Edge> findEdgeBySource(String source);
+	
+	/**
+	 * Method to find edges based on target node.
+	 * 
+	 * @param target The target node.
+	 */
+	@Modifying
+    @Transactional
+    @Query(value = "SELECT e FROM edges e WHERE e.target = ?1", nativeQuery = true)
+    List<Edge> findEdgeByTarget(String target);
+	
+	/**
+	 * Method to find edge based on source and target node.
+	 * 
+	 * @param source The source node.
+	 * @param target The target node.
+	 */
+	@Modifying
+    @Transactional
+    @Query(value = "SELECT e FROM edges e WHERE e.source = ?1 AND e.target = ?2", nativeQuery = true)
+    Optional<Edge> findEdgeBySourceAndTarget(String source, String target);
+	
+	/**
+	 * Method to find edge based on target and source node.
+	 * 
+	 * @param target The target node.
+	 * @param source The source node.
+	 */
+	@Modifying
+    @Transactional
+    @Query(value = "SELECT e FROM edges e WHERE e.target = ?1 AND e.source = ?2", nativeQuery = true)
+    Optional<Edge> findEdgeByTargetAndSource(String target, String source);
+	
+	/**
+	 * Method to delete an edge where the specified node is a source.
+	 * 
+	 * @param name The node being removed.
+	 */
+	@Modifying
+    @Transactional
+    @Query(value = "DELETE FROM edges e WHERE e.source = ?1", nativeQuery = true)
+    void deleteEdgeBySource(String name);
+	
+	/**
+	 * Method to delete an edge where the specified node is a target.
+	 * 
+	 * @param name The node being removed.
+	 */
+	@Modifying
+    @Transactional
+    @Query(value = "DELETE FROM edges e WHERE e.target = ?1", nativeQuery = true)
+    void deleteEdgeByTarget(String name);
 }
--- src/main/java/com/mjh/innaxyswebapp/data/NodeRepository.java
+++ src/main/java/com/mjh/innaxyswebapp/data/NodeRepository.java
@@ -1,6 +1,9 @@
 package com.mjh.innaxyswebapp.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.mjh.innaxyswebapp.model.Node;
 
@@ -15,5 +18,22 @@ import com.mjh.innaxyswebapp.model.Node;
  * @param <String> Primary key of a relevant node (name of the node).
  */
 public interface NodeRepository extends JpaRepository<Node, String> {
-
+	/**
+	 * Method to delete an edge where the specified node is a source or target.
+	 * 
+	 * @param name The node being removed.
+	 */
+	@Modifying
+    @Transactional
+    @Query(value = "DELETE FROM edges e WHERE e.source = ?1 OR e.target = ?1", nativeQuery = true)
+    void deleteEdgeByNode(String name);
+	
+	/**
+	 * Method to remove all edges from the database - typically used when all nodes
+	 * are first removed.
+	 */
+	@Modifying
+    @Transactional
+    @Query(value = "TRUNCATE TABLE edges", nativeQuery = true)
+    void deleteAllEdges();
 }
