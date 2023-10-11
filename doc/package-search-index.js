8ee74878e77ab62298a8a44dbefa1b563c5ccaf2
MaxMJH <MaxHarrisMJH@gmail.com>
2023-10-11 17:44:33 +0100

    Updated JavaDoc for the application.

--- doc/package-search-index.js
+++ doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.mjh.innaxyswebapp.model"},{"l":"com.mjh.innaxyswebapp.utility"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.mjh.innaxyswebapp"},{"l":"com.mjh.innaxyswebapp.controller.api"},{"l":"com.mjh.innaxyswebapp.controller.web"},{"l":"com.mjh.innaxyswebapp.data"},{"l":"com.mjh.innaxyswebapp.model"},{"l":"com.mjh.innaxyswebapp.service"},{"l":"com.mjh.innaxyswebapp.utility"}];updateSearchResults();
